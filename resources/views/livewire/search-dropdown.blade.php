ececa260eab853da9e294d41d1cce1f6d80d309d
Qing Ma <gsmofgsm@hotmail.com>
2020-12-29 23:02:16 +0100

    autofocus search dropdown on keydown /

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -4,6 +4,13 @@
         type="text"
         class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
         placeholder="Search"
+        x-ref="search"
+        @keydown.window="
+            if (event.keyCode === 191) {
+                event.preventDefault();
+                $refs.search.focus();
+            }
+        "
         @focus="isOpen = true"
         @keydown="isOpen = true"
         @keydown.escape.window="isOpen = false"
