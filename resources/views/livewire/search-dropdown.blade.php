941045903853936c1b56a4e043f891fd42a18904
Qing Ma <gsmofgsm@hotmail.com>
2020-12-29 19:22:15 +0100

    spinner

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -3,6 +3,9 @@
     <div class="absolute top-0 mt-2 ml-2">
         <svg class="fill-current w-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"/></svg>
     </div>
+
+    <div wire:loading class="spinner top-0 right-0 mt-3 mr-4"></div>
+
     @if (strlen($search) >= 2)
         <div class="absolute bg-gray-800 rounded w-64 mt-4">
             <ul>
