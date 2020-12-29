2d4be10c698049b44230602a0af4212fe2bc6814
Qing Ma <gsmofgsm@hotmail.com>
2020-12-29 19:49:45 +0100

    interactivity search dropdown with alpine.js

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,6 +11,7 @@
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <livewire:styles />
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </head>
 <body class="font-sans bg-gray-900 text-white">
     <nav class="border-b border-gray-800">
--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,5 +1,14 @@
-<div class="relative mt-3 md:mt-0">
-    <input wire:model.debouce.500ms="search" type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
+<div class="relative mt-3 md:mt-0" x-data="{ isOpen: true }" @click.away="isOpen = false">
+    <input
+        wire:model.debouce.500ms="search"
+        type="text"
+        class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
+        placeholder="Search"
+        @focus="isOpen = true"
+        @keydown="isOpen = true"
+        @keydown.escape.window="isOpen = false"
+        @keydown.shift.tab="isOpen = false"
+    >
     <div class="absolute top-0 mt-2 ml-2">
         <svg class="fill-current w-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"/></svg>
     </div>
@@ -7,12 +16,20 @@
     <div wire:loading class="spinner top-0 right-0 mt-3 mr-4"></div>
 
     @if (strlen($search) >= 2)
-        <div class="absolute bg-gray-800 rounded w-64 mt-4">
+        <div
+            class="z-50 absolute bg-gray-800 rounded w-64 mt-4"
+            x-show.transition.opacity="isOpen"
+        >
             <ul>
                 @forelse($searchResults as $result)
                     <li class="border-b border-gray-700">
-                        <a href="{{ route('movies.show', $result['id']) }}" class="block hover:bg-gray-700
-                        px-3 py-3 flex items-center">
+                        <a
+                            href="{{ route('movies.show', $result['id']) }}"
+                            class="block hover:bg-gray-700 px-3 py-3 flex items-center transition ease-in-out duration-150"
+                            @if ($loop->last)
+                                @keydown.tab="isOpen = false"
+                            @endif
+                        >
                             @if($result['poster_path'])
                                 <img src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt="poster" class="w-8">
                             @else
