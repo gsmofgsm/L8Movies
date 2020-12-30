20d3b609f0d73234eed0286596d500c67b1879ab
Qing Ma <gsmofgsm@hotmail.com>
2020-12-30 20:27:25 +0100

    tv show

--- app/Http/Controllers/TvController.php
+++ app/Http/Controllers/TvController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\ViewModels\MovieViewModel;
+use App\ViewModels\TvShowViewModel;
 use App\ViewModels\TvViewModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
@@ -60,7 +62,11 @@ class TvController extends Controller
      */
     public function show($id)
     {
-        return view('tv.show');
+        $tvshow = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/tv/' . $id . '?append_to_response=credits,videos,images')
+            ->json();
+        $viewModel = new TvShowViewModel($tvshow);
+        return view('tv.show', $viewModel);
     }
 
     /**
